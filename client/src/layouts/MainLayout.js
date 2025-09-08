ffed8f6a47a930cb7835edb147af4a35089a4050
Harish Choudhary <94277296+ChoudharyHarish@users.noreply.github.com>
2025-09-09 02:05:40 +0530

    Update MainLayout.js

--- client/src/layouts/MainLayout.js
+++ client/src/layouts/MainLayout.js
@@ -11,12 +11,15 @@ import TopBar from "../components/Navbar/TopBar";
 const MainLayout = (props) => {
   const { expanded, addPadding } = props;
   const { createModalOpen } = useSelector((state) => state.user);
+
+  console.log(process.env.REACT_APP_API_URL);
   return (
     <>
       <div className="flex">
         <Suspense>
           <div className="flex flex-col md:flex-row w-full">
             <SideBar expanded={expanded} />
+            <h1>{process.env.REACT_APP_API_URL}</h1>
             <TopBar />
             <div
               className={`mt-[73px] md:mt-0 w-full ${
